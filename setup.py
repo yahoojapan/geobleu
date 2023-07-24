015aaec20c30c8b2608b7006f5e6ad2aaa495c7b
Toru Shimizu <toshimiz@yahoo-corp.jp>
2023-07-24 11:30:50 +0900

    fix attribute value

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='geobleu',
       version='0.3',
       description='Implementation of GEO-BLEU and other evaluation metrics for HuMob Challenge 2023',
-      url='https://ghe.corp.yahoo.co.jp/toshimiz/geobleu',
+      url='https://github.com/yahoojapan/geobleu',
       author='Toru Shimizu, Yahoo Japan Corporation',
       author_email='toshimiz@yahoo-corp.jp',
       license='MIT',
