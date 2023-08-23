6911caf71d41fce40c0d8978febb224ebd867a0e
Toru Shimizu <toshimiz@yahoo-corp.jp>
2023-08-23 15:17:45 +0900

    restore non-parallel methods

--- geobleu/__init__.py
+++ geobleu/__init__.py
@@ -1,4 +1,6 @@
 from .seq_eval import calc_geobleu_orig
 from .seq_eval import calc_geobleu
+from .seq_eval import calc_geobleu_single
 from .seq_eval import calc_dtw_orig
 from .seq_eval import calc_dtw
+from .seq_eval import calc_dtw_single
