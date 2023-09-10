cdaa3647f3e07df2f6c9b8417c8cf97fb7fafef9
Toru Shimizu <toshimiz@yahoo-corp.jp>
2023-09-10 23:58:02 +0900

    update the self-check tool

--- validator.py
+++ validator.py
@@ -19,22 +19,75 @@ task_specs = {
         "coord_min": 1,
         "coord_max": 200,
     },
+    "test": {
+        "uid_range": (50, 60),
+        "d_min": 60,
+        "d_max": 74,
+        "t_min": 0,
+        "t_max": 47,
+        "coord_min": 1,
+        "coord_max": 200,
+    },
 }
 
 def error(message):
     print(message)
     sys.exit(1)
 
+def load_dataset(fpath, specs):
+    uid_dict = dict()
+    for l in open(fpath):
+        if l.startswith("uid"):
+            continue
+        l = l.rstrip()
+        uid_str, d_str, t_str, x_str, y_str = l.split(",")
+        uid = int(uid_str)
+        if uid < specs["uid_range"][0]:
+            continue
+        d = int(d_str)
+        t = int(t_str)
+        x = int(x_str)
+        y = int(y_str)
+
+        if uid not in uid_dict.keys():
+            uid_dict[uid] = list()
+        if d >= specs["d_min"]:
+            uid_dict[uid].append((d, t, x, y))
+
+    return uid_dict
+
+def check_consistency(pred_seq, ans_seq, uid):
+    # check the consistency between a trajectory in prediction and its counterpart in reference
+    error_prefix = "Error occurring regarding uid {}: ".format(uid)
+
+    # the trajectory length
+    if len(pred_seq) != len(ans_seq):
+        error(
+            error_prefix + \
+            "The length doesn't match between the generated and reference trajectories.")
+
+    # consistency of day and time
+    for idx, (pred_step, ans_step) in enumerate(zip(pred_seq, ans_seq)):
+        pred_d, pred_t = pred_step[:2]
+        ans_d, ans_t = ans_step[:2]
+        if not (pred_d == ans_d and pred_t == ans_t):
+            error(
+                error_prefix + \
+                "Day and time are not the same; "
+                "(d, t) = ({}, {}) for generated while (d, t) = ({}, {}) for reference at step {} of the trajectory.".format(
+                    pred_d, pred_t, ans_d, ans_t, idx))
+
 def main():
     # parse the arguments
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         error(
             "Usage: \n"
-            "    python3 validator.py task_id submission_file_path\n"
+            "    python3 validator.py task_id dataset_file_path submission_file_path\n"
             "        where task_id is either 1 or 2")
 
     task_id = sys.argv[1]
-    fpath = sys.argv[2]
+    dataset_fpath = sys.argv[2]
+    generated_fpath = sys.argv[3]
 
     if task_id not in task_specs.keys():
         error("Invalid task_id: {}".format(task_id))
@@ -57,8 +110,11 @@ def main():
 
     # now start the actual test...
     uid_set = set()
+    pred_uid_dict = dict()
+    ans_uid_dict = dict()
 
-    for i, l in enumerate(open(fpath)):
+    print("Loading the submission file...")
+    for i, l in enumerate(open(generated_fpath)):
         if i == 0 and l.startswith("uid,"):
             # skip the header line
             continue
@@ -108,11 +164,28 @@ def main():
                 error_prefix + \
                 "y={} is out of range".format(y))
 
+        if uid not in pred_uid_dict.keys():
+            pred_uid_dict[uid] = list()
+        pred_uid_dict[uid].append((d, t, x, y))
+    print("")
+
     # uid check
+    print("Checking the set of uid's...")
     if uid_set != uid_set_ref:
         error(
             "The set of uid's doesn't match that of reference; "
             "there seem to be extra or lacking uid's")
+    print("")
+
+    # comparison between the submission file and the dataset
+    print("Now loading the dataset file and comparing the submission data to it...")
+    ans_uid_dict = load_dataset(dataset_fpath, specs)
+    for uid in range(specs["uid_range"][0], specs["uid_range"][1]):
+        pred_seq = pred_uid_dict[uid]
+        ans_seq = ans_uid_dict[uid]
+        check_consistency(pred_seq, ans_seq, uid)
+    print("")
+
 
     print("Validation finished without errors!")
 
